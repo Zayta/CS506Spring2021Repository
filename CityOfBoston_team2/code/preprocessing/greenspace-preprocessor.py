1c6975ffb77a7079a0e491a41e8a8d7145922dd2
Sandra Zhen <szhen8930@gmail.com>
2021-03-08 12:49:35 -0500

    added surface analysis.md

--- CityOfBoston_team2/code/preprocessing/greenspace-preprocessor.py
+++ CityOfBoston_team2/code/preprocessing/greenspace-preprocessor.py
@@ -4,7 +4,7 @@ import difflib
 import re
 
 def import_data(filename):
-    data = pd.read_csv(filename, sep=',', usecols = ['DISTRICT','ZonAgg','TypeLong','ACRES','ADDRESS','ShapeSTArea','ShapeSTLength'])
+    data = pd.read_csv(filename, sep=',', usecols = ['SITE_NAME','OWNERSHIP','DISTRICT','ZonAgg','TypeLong','ACRES','ADDRESS','ShapeSTArea','ShapeSTLength'])
     return data
 
 #https://stackoverflow.com/questions/61858903/remove-duplicate-approximate-word-matching-using-fuzzy-python
@@ -36,7 +36,7 @@ def output_data(dataframe,path):
 df = import_data("../../datasets/open_space.csv")
 df['TypeLong'] = similarity_replace(df.TypeLong)
 
-print('Types are: ', columnValues(df,'TypeLong'))
-
 output_data(df,"../../datasets/open_space_sanitized.csv")
+
+
 
